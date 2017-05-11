867323fcd4af1fb0314817808abb703081bca67a
Maximilian J. Zangs <maximilian.zangs@gmail.com>
2017-05-11 14:33:18 +0100

    Checks if xtensa needs installation

--- install.sh
+++ install.sh
@@ -2,7 +2,11 @@
 
 MPY_INSTALL="mpy-esp8266-install"
 
-git submodule update --init
-cd $MPY_INSTALL
-. install.sh
-cd ..
+if [[ ":$PATH:" == *"/xtensa-lx106-elf/bin:"  ]]; then
+	echo "xtensa compiler already installed"
+else
+	git submodule update --init
+	cd $MPY_INSTALL
+	. install.sh
+	cd ..
+fi
